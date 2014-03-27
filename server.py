9876a424065f4f008ab2ea07c3bef7acdf4a4d78
Justin Rucinski <rucins11@msu.edu>
2014-03-27 09:35:22 -0400

    added chat and quotes functionality

--- server.py
+++ server.py
@@ -8,6 +8,8 @@ import quixote
 import imageapp
 import argparse
 
+from quotes import apps as quotesapp
+from chat import apps as chatapp
 from app import make_app
 from quixote.demo import create_publisher
 # from quixote.demo.mini_demo import create_publisher
@@ -15,11 +17,12 @@ from quixote.demo import create_publisher
 # from wsgiref.validate import validator
 
 def main():
-    s = socket.socket() # Create a socket object.
-    host = socket.getfqdn() # Get local machine name.
 
+    s = socket.socket()         # Create a socket object
+    host = socket.getfqdn() # Get local machine name
+    
     parser = argparse.ArgumentParser() #creating a parser 
-    parser.add_argument("-A", choices=['image', 'altdemo', 'myapp'],
+    parser.add_argument("-A", choices=['image', 'altdemo', 'myapp', 'quotes', 'chat'],
             help='Choose which app you would like to run')
     parser.add_argument("-p", type=int, help="Choose the port you would like to run on.")
     args = parser.parse_args()
@@ -27,10 +30,10 @@ def main():
     #Check to see if a port is specified
     if args.p == None:
         port = random.randint(8000, 9999) #Creating WSGI app
-        s.bind((host, port))
+        s.bind((host, port))        # Bind to the port
     else:
         port = args.p
-        s.bind((host, port))
+        s.bind((host, port))        # Bind to the port
     if args.A == 'myapp':
         wsgi_app = make_app()
     elif args.A == "image":
@@ -40,6 +43,10 @@ def main():
     elif args.A == "altdemo":
         p = create_publisher()
         wsgi_app = quixote.get_wsgi_app()
+    elif args.A == "quotes":
+        wsgi_app = quotesapp.QuotesApp('quotes/quotes.txt','./quotes/html')
+    elif args.A == "chat":
+        wsgi_app = chatapp.ChatApp('./chat/html')
     else:
         wsgi_app = make_app() #In the event that no argument is passed just make my_app
 
